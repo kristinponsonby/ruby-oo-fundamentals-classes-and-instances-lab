af7d585d053f2ef78e4bc00cf1c8a852a099f9f9
Kristin <kristinponsonby@gmail.com>
2021-02-15 12:54:37 -0500

    commit tree 6960a54ac3435e57c2dd6a64dd9b2febad6d4769

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
+
+
+
+
+
+
+# Checklist
+# {x} Define the class "Dog"
+# {x} Define a local variable 'fido' as an instance of the class 'Dog'
+# {} Define a second local variable 'snoopy' as an instance of the class Dog
+# {} Define a third local variable 'lassie' as an instance of the class Dog
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
+
+
+
+
+# Checklist
+# {x} Define the class "Person"
+# {x} Define a local variable 'adele_goldberg' as an instance of the class 'Person'
+# {x} Define a second local variable 'alan_kay' as an instance of the class Person
